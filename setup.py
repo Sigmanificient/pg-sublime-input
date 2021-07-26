9991b272dee0315d15693a4a2ec00c6060eed657
Sigmanificient <edhyjox@gmail.com>
2021-07-26 14:16:45 +0200

    Added typing and documentation, completed setup file.

---
+++ setup.py
@@ -0,0 +1,26 @@
+import pathlib
+from setuptools import setup
+
+HERE: pathlib.Path = pathlib.Path(__file__).parent
+README: str = (HERE / "README.md").read_text()
+
+setup(
+    name="sublinput",
+    version="1.0.0",
+    description="An input pygame alternative for sublime text users.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Edhyjox/impauto",
+    author="Sigmanificient",
+    author_email="Edhyjox@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["sublinput"],
+    include_package_data=True,
+    install_requires=[]
+)
