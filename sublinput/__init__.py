c4122ccf7790b359b1b8b4388e73e2d8618c525f
Sigmanificient <edhyjox@gmail.com>
2021-07-26 14:16:45 +0200

    Added typing and documentation, completed setup file.

--- sublinput/__init__.py
+++ sublinput/__init__.py
@@ -1,8 +1,11 @@
+"""An input pygame alternative for sublime text users."""
+
 from sublinput.ui import InputBoxScreen
 import builtins
 
 
-def new_input(__prompt='') -> str:
+def new_input(__prompt: str ='') -> str:
+    """A function that creates a new input box screen and return it's value."""
     with InputBoxScreen() as input_box:
         return input_box(__prompt)
 
--- sublinput/ui.py
+++ sublinput/ui.py
@@ -1,40 +1,51 @@
-import os
+"""Handle the whole user interface of the sublinput package."""
 
-os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
+from __future__ import annotations
+
+import os
+from typing import Union, Tuple, Optional, Any
+
+# Hides the pygame support message
+os.environ['PYGAME_HIDE_SUPPORT_PROMPT']: str = "hide"
 
 import pygame
+
+Color = Union[Tuple[int, int, int], Tuple[int, int, int, int]]
 
 pygame.init()
 pygame.display.init()
 
-HEIGHT = 24
-WIDTH = 480
+HEIGHT: int = 24
+WIDTH: int = 480
 
-WHITE = (255,) * 3
-BLACK = (0,) * 3
-ALPHA = (0,) * 4
+WHITE: Color = (255,) * 3
+BLACK: Color = (0,) * 3
+ALPHA: Color = (0,) * 4
 
-SCREEN_TEXT_OFFSET = 5
+SCREEN_TEXT_OFFSET: int = 5
 
-ICON = pygame.Surface((64, 64), pygame.SRCALPHA)
+ICON: pygame.Surface = pygame.Surface((64, 64), pygame.SRCALPHA)
 ICON.fill(ALPHA)
 
-FONT = pygame.font.SysFont('Consolas', 16)
+FONT: pygame.font.SysFont = pygame.font.SysFont('Consolas', 16)
 
 
 class InputBoxScreen:
 
     def __init__(self) -> None:
-        self.screen = None
-        self.is_running = True
-        self.result_value = ''
+        """Initialize the input box screen."""
+        self.screen: Optional[pygame.display] = None
+        self.is_running: bool = True
+        self.result_value: str = ''
 
-    def __enter__(self):
+    def __enter__(self) -> InputBoxScreen:
+        """Initialize pygame display."""
         pygame.display.init()
         return self
 
     def __call__(self, __prompt: str) -> str:
-        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        """Create a screen and get a value from the user."""
+        self.screen: pygame.display = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption(__prompt)
         pygame.display.set_icon(ICON)
 
@@ -45,15 +56,17 @@ class InputBoxScreen:
         print(f"{__prompt}{self.result_value}")
         return self.result_value
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    def __exit__(self, exc_type: Any, exc_val: Any, exc_tb: Any) -> None:
+        """QUit pygame display and raise possible exception."""
         pygame.display.quit()
 
         if exc_type:
             raise
 
-    def handle_event(self, event):
+    def handle_event(self, event: pygame.Event) -> None:
+        """Handle event from the user."""
         if event.type == pygame.QUIT:
-            self.is_running = False
+            self.is_running: bool = False
             return
 
         if event.type == pygame.TEXTINPUT:
@@ -64,21 +77,22 @@ class InputBoxScreen:
             return
 
         if event.key == pygame.K_BACKSPACE:
-            self.result_value = self.result_value[:-1]
+            self.result_value: str = self.result_value[:-1]
             return
 
         if event.key == pygame.K_RETURN:
-            self.is_running = False
+            self.is_running: bool = False
             return
 
-    def draw(self):
+    def draw(self) -> None:
+        """Render the user text on the screen."""
         self.screen.fill(BLACK)
-        text = FONT.render(self.result_value, True, WHITE)
-
-        text_rect = text.get_rect()
+        text: pygame.Surface = FONT.render(self.result_value, True, WHITE)
+        text_rect: pygame.Rect = text.get_rect()
 
         text_rect.top = SCREEN_TEXT_OFFSET
 
+        # Scroll when value length is higher then screen size
         if text_rect.width < (WIDTH - SCREEN_TEXT_OFFSET):
             text_rect.left = SCREEN_TEXT_OFFSET
         else:
@@ -88,7 +102,8 @@ class InputBoxScreen:
         pygame.display.update()
 
 
-def main():
+def main() -> None:
+    """A Demo of the InputBoxScreen class."""
     with InputBoxScreen() as input_box_demo:
         input_box_demo("Demo: ")
 
